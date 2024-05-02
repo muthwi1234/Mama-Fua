e9fb69fb7dd1af205e7d674e6ec8c49318e5ae81
peter muthwi <peterkilungya1@gmail.com>
2024-05-02 09:19:19 +0300

    footer done

--- Fua/src/Components/Footer.jsx
+++ Fua/src/Components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+// MamaFuaFooter.js
+import React from 'react';
 
-function Footer() {
+const MamaFuaFooter = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-900 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">&copy; 2024 Mama Fua. All rights reserved.</p>
+        <p className="text-sm mt-2">Designed and Developed by pinchez</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default MamaFuaFooter;
